9deac08cfc40e035cb266983e6d875c3ece68a38
Shakiv Hussain <51313301+shakivhussain@users.noreply.github.com>
2023-05-12 01:13:19 +0530

    ✅ States

--- app/src/main/java/com/shakiv/husain/wellnessapp/MainActivity.kt
+++ app/src/main/java/com/shakiv/husain/wellnessapp/MainActivity.kt
@@ -3,13 +3,28 @@ package com.shakiv.husain.wellnessapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.shakiv.husain.wellnessapp.ui.theme.WellnessAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,9 +32,147 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             WellnessAppTheme {
-
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    WellnessScreen()
+                }
             }
         }
     }
 }
 
+
+@Composable
+fun WellnessScreen(modifier: Modifier = Modifier) {
+    WaterCounter()
+}
+
+@Composable
+fun WaterCounter(modifier: Modifier = Modifier) {
+
+    Column(modifier = modifier.padding(16.dp)) {
+
+        var count by remember { mutableStateOf(0) }
+
+        if (count > 0) {
+            var showTask by remember { mutableStateOf(true) }
+            if (showTask) {
+                WellnessTaskItem(taskName = "Have you taken your 15 minute walk today?",
+                    onClose = {
+                        showTask = false
+                    },
+                    checked = true,
+                    onCheckedChange = {})
+            }
+            Text(text = "You've had $count glasses.")
+        }
+
+        Row(Modifier.padding(16.dp)) {
+
+            Button(
+                onClick = { count++ },
+                modifier = Modifier
+                    .padding(8.dp),
+                enabled = count < 10
+            ) {
+                Text(text = "Add One")
+            }
+
+
+            Button(
+                onClick = { count = 0 },
+                Modifier.padding(8.dp)
+            ) {
+                Text(text = "Clear Water Count")
+
+            }
+
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun WellnessTaskItemPreview() {
+    WellnessTaskItem(
+        taskName = "Shakib",
+        onClose = {},
+        checked = true,
+        onCheckedChange = {}
+    )
+}
+
+@Composable
+fun WellnessTaskItem(
+    taskName: String,
+    onClose: () -> Unit,
+    checked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier.padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+        Text(
+            text = taskName, modifier = Modifier
+                .weight(1F)
+                .padding(start = 8.dp)
+        )
+
+        Checkbox(checked = checked, onCheckedChange = onCheckedChange,  modifier = Modifier
+            .weight(.5F)
+            .padding(start = 8.dp))
+        Icon(
+            imageVector = Icons.Filled.Close,
+            contentDescription = null,
+            modifier = Modifier.clickable {
+                onClose()
+            }
+        )
+
+    }
+
+}
+
+
+@Composable
+fun StateLessComposable(
+    count: Int,
+    onIncrement: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = Modifier) {
+        if (count > 0) {
+            Text(text = "You've had $count Glasses.")
+        }
+
+        Button(
+            onClick = { onIncrement.invoke() },
+            modifier = Modifier.padding(top = 8.dp),
+            enabled = count < 10
+        ) {
+            Text(text = "Add one")
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun StateFullComposable(modifier: Modifier = Modifier) {
+
+    var waterCount by remember { mutableStateOf(0) }
+    var juiceCount by remember { mutableStateOf(0) }
+
+    Column {
+
+        StateLessComposable(count = waterCount, onIncrement = { waterCount++ }, modifier = modifier)
+        StateLessComposable(count = juiceCount, onIncrement = { juiceCount++ }, modifier = modifier)
+    }
+
+}
